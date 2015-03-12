5982601db7ab1ffa5069884fa4ba0ebb6ffd2a16
Roger Lam <mrlamroger@gmail.com>
2015-03-12 00:23:56 -0700

    Formatted with go fmt

--- go/problem_001.go
+++ go/problem_001.go
@@ -10,9 +10,9 @@ import "fmt"
 func main() {
 	sum := 0
 	for i := 1; i < 1000; i++ {
-		if i % 3 == 0 || i % 5 == 0 {
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
-		}	
-	} 
+		}
+	}
 	fmt.Println(sum)
 }
